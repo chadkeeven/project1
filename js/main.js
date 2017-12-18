1dc3b2c1d7838e0a07cc4a8879435e2886500a5b
chadkeeven <chadkeeven@gmail.com>
2017-12-17 20:13:04 -0700

    comments were made
    Comments to explain why i made the changes that i did

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ var uiPlay = $("#gamePlay");
 var uiTimer = $("#timer");
 
 var matchingGame = {};
+//made deck only have one card instead of 2 for the loop to run quicker.
 matchingGame.deck = ['nopales', 'grilledfish', 'tripa', 'bajafish', 'barbacoa', 'carneasada', 'carnitas', 'chorizoasado', 'shrimptaco', 'decabeza', 'alpastor', 'dorados', 'lengua', 'chicharron', 'sudados', 'polloasado'];
 
 $(function() {
@@ -40,27 +41,29 @@ function startGame() {
     uiScore.html("0 seconds");
     uiStats.show();
     uiCards.show();
+    //Holds the cards that will be used for play
     var masterDeck = [];
+    //Makes sure no duplicate pairs are being used
     var numbersUsed = [];
     score = 0;
     cardsmatched = 0;
     if (playGame == false) {
         playGame = true;
-        var n = 0;
+        var n = 0; //amount of cards we want to pull from the matchingGame Deck
         while ( n < 12) {
-            var randomCard = Math.floor(Math.random() * (16 - 0) + 0);
+            var randomCard = Math.floor(Math.random() * (16 - 0) + 0); //creates a random card number between 0-15
             console.log(numbersUsed);
-            if ( numbersUsed.includes(randomCard) == false) {
-                numbersUsed.push(randomCard);
-                masterDeck.push(matchingGame.deck[randomCard]);
-                masterDeck.push(matchingGame.deck[randomCard]);
+            if ( numbersUsed.includes(randomCard) == false) { //checks if random number has already been used
+                numbersUsed.push(randomCard); //pushes number to array so won't be used again
+                masterDeck.push(matchingGame.deck[randomCard]); //push one of the cards to the deck
+                masterDeck.push(matchingGame.deck[randomCard]); //push the same card to make sure there is a match
                 console.log(masterDeck);
-                n++;  
+                n++;  //only counts if 2 cards have been added that haven't already been added
             }
         }
 
 
-        var shuffledDeck = masterDeck.sort(shuffle);
+        var shuffledDeck = masterDeck.sort(shuffle);  //shuffle the deck to get it out of order
         for (var i = 0; i < 23; i++) {
             $(".card:first-child").clone().appendTo("#cards");
             //   console.log (uiCards);
@@ -72,7 +75,7 @@ function startGame() {
                 "left": ($(this).width() + 20) * (index % 6),
                 "top": ($(this).height() + 20) * Math.floor(index / 6)
             });
-            var pattern = masterDeck.pop();
+            var pattern = masterDeck.pop();//pop the masterdeck, the orginal deck isn't touched.
             // console.log(pattern);
             $(this).find(".back").addClass(pattern);
             $(this).attr("data-pattern", pattern);
@@ -145,6 +148,7 @@ function reStartGame() {
     uiCards.html("<div class='card'><div class='face front'></div><div class='face back'></div></div>");
     clearTimeout(scoreTimeout);
     console.log(matchingGame.deck);
+    //don't need since the matchingGame deck isn't manipulated.
    // matchingGame.deck = ['nopales', 'nopales', 'grilledfish', 'grilledfish', 'tripa', 'tripa', 'bajafish', 'bajafish', 'barbacoa', 'barbacoa', 'carneasada', 'carneasada', 'carnitas', 'carnitas', 'chorizoasado', 'chorizoasado', 'shrimptaco', 'shrimptaco', 'decabeza', 'decabeza', 'alpastor', 'alpastor', 'dorados', 'dorados', 'lengua', 'lengua', 'chicharron', 'chicharron', 'sudados', 'sudados', 'polloasado', 'polloasado', ];
     startGame();
 }
